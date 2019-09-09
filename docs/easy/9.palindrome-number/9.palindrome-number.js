20581f4ffc53968afbe7878d28e56db0ffb8f349
LeviDing <imdingxuewen@gmail.com>
2019-09-09 18:44:19 +0800

    :art: Improving format of the code

--- docs/easy/9.palindrome-number/9.palindrome-number.js
+++ docs/easy/9.palindrome-number/9.palindrome-number.js
@@ -28,13 +28,11 @@ var isPalindrome = function(x) {
     if (x < 0 || x % 10 === 0 && x !== 0) {
         return false;
     }
-
     let revertedNumber = 0;
     while (x > revertedNumber) {
         revertedNumber = revertedNumber * 10 + x % 10;
         x = Math.floor(x / 10);
     }
-
     return x === revertedNumber || x === Math.floor(revertedNumber / 10);
     //return (x === revertedNumber || (x * 10 + revertedNumber % 10) === revertedNumber);
 };
